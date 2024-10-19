104a2ac393fed58c3376a2277fb95aa9c2f89e9a
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-10-20 00:19:23 +0800

    Array Activity

---
+++ arrayActivity.js
@@ -0,0 +1,14 @@
+
+let favSingers = ["Ben&Ben", "Bruno Mars", "Billie Eilish"];
+
+console.log("First favorite singer:", favSingers[0]);
+
+let favNumbers = [1, 2, 3, 4];
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log("Mixed array items:");
+console.log(mixedArr[0]); 
+console.log(mixedArr[1]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]);
